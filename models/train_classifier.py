088f98a305da4d6ecc7ad39c4230b23e4a6e0770
wythe0513 <wythenshawe0513@gmail.com>
2020-11-23 00:26:32 +0900

    modify train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -99,7 +99,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
     """
     Y_pred = model.predict(X_test)
 
-    print(classification_report(Y_test, Y_pred, target_names=category_names)
+    print(classification_report(Y_test, Y_pred, target_names=category_names))
 
 
 def save_model(model, model_filepath):
